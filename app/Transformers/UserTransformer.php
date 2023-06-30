6ed8a79646e6d5d71b16a7ca52f3bcf879535cdc
hungnguyen251 <hungnguyen.cfos@gmail.com>
2023-06-30 22:00:27 +0700

    Add fractal lib to data transformer

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected array $defaultIncludes = [];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @param  \App\Models\User  $user
+     * @return array
+     */
+    public function transform(User $user): array
+    {
+        return [
+            'id'    => $user->id,
+            'name'  => $user->name,
+            'email' => $user->email,
+        ];
+    }
+}
